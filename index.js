07fbd372cec235a813a9ce803a31c252437de6ec
KristinaSolo <97984146+KristinaSolo@users.noreply.github.com>
2022-05-17 15:13:23 +0300

    wek15

---
+++ index.js
@@ -0,0 +1,21 @@
+let colorDiv = document.getElementById('block-color');
+// console.log(colorDiv);
+
+
+function addColor(sender){
+
+
+// console.log(sender.value)
+
+colorDiv.setAttribute('class', sender.value);
+
+}
+
+
+
+
+
+
+
+
+
